5ce34e3082eb147cc9939c2fa987d3bfbc2f1f79
Kyle Conroy <kyleconroy@gmail.com>
2023-05-12 13:14:42 -0400

    infrastructure for multiple-ephems
    can test by manually forcing component options (until the UI is developed for adding components and UI/API for renaming/deleting):
    ```
    ephem = lcviz.plugins['Ephemeris']
    ephem.component._manual_options = ['default', 'new']
    ephem.component.items = [{"label": opt} for opt in ['default', 'new']]
    ```

--- lcviz/plugins/ephemeris/ephemeris.py
+++ lcviz/plugins/ephemeris/ephemeris.py
@@ -29,6 +29,9 @@ class Ephemeris(PluginTemplateMixin):
       Period of the ephemeris, defined at ``t0``.
     * :attr:`dpdt`:
       First derivative of the period of the ephemeris.
+    * :meth:`ephemeris`
+    * :meth:`ephemerides`
+    * :meth:`update_ephemeris`
     * :meth:`create_phase_viewer`
 
     """
@@ -49,19 +52,26 @@ class Ephemeris(PluginTemplateMixin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self._ignore_ephem_change = False
+        self._ephemerides = {}
+
         # TODO: support renaming components (including renaming the applicable viewer)
         # TODO: support for creating new components
         self.component = SelectPluginComponent(self,
                                                items='component_items',
                                                selected='component_selected',
                                                manual_options=['default'])
+        # force the original entry in ephemerides with defaults
+        self._change_component()
 
         self.hub.subscribe(self, ViewerAddedMessage, handler=self._check_if_phase_viewer_exists)
         self.hub.subscribe(self, ViewerRemovedMessage, handler=self._check_if_phase_viewer_exists)
 
     @property
     def user_api(self):
-        expose = ['component', 'period', 'dpdt', 't0', 'create_phase_viewer']
+        expose = ['component', 'period', 'dpdt', 't0',
+                  'ephemeris', 'ephemerides',
+                  'update_ephemeris', 'create_phase_viewer']
         return PluginUserApi(self, expose=expose)
 
     @property
@@ -71,6 +81,14 @@ class Ephemeris(PluginTemplateMixin):
     @property
     def phase_viewer_id(self):
         return f'flux-vs-phase:{self.component_selected}'
+
+    @property
+    def ephemerides(self):
+        return self._ephemerides
+
+    @property
+    def ephemeris(self):
+        return self.ephemerides.get(self.component_selected, {})
 
     def _update_all_phase_arrays(self):
         dc = self.app.data_collection
@@ -133,12 +151,72 @@ class Ephemeris(PluginTemplateMixin):
     def vue_period_double(self, *args):
         self.period *= 2
 
-    @observe('component_selected')
     def _check_if_phase_viewer_exists(self, *args):
         self.phase_viewer_exists = self.phase_viewer_id in self.app.get_viewer_ids()
 
+    @observe('component_selected')
+    def _change_component(self, *args):
+        if not hasattr(self, 'component'):
+            # plugin/traitlet startup
+            return
+        self._check_if_phase_viewer_exists()
+        ephem = self._ephemerides.get(self.component_selected, {})
+
+        # we'll temporarily disable updating the phasing so that we can set all
+        # traitlets simultaneously and THEN revising phase arrays if necessary
+        self._ignore_ephem_change = True
+        self.t0 = ephem.get('t0', self.t0)
+        self.period = ephem.get('period', self.period)
+        self.dpdt = ephem.get('dpdt', self.dpdt)
+
+        # if this is a new component, update those default values back to the dictionary
+        self.update_ephemeris(t0=self.t0, period=self.period, dpdt=self.dpdt)
+        self._ignore_ephem_change = False
+        if ephem:
+            # if there were any changes applied by accessing the dictionary,
+            # then we need to update phasing, etc (since we set _ignore_ephem_change)
+            # otherwise, this is a new component and there is no need.
+            self._ephem_traitlet_changed()
+
+    def update_ephemeris(self, component=None, t0=None, period=None, dpdt=None):
+        """
+        Update the ephemeris for a given component.
+
+        Parameters
+        ----------
+        component : str, optional
+            label of the component.  If not provided or ``None``, will default to plugin value.
+        t0 : float, optional
+            value of t0 to replace
+        period : float, optional
+            value of period to replace
+        dpdt : float, optional
+            value of period to replace
+
+        Returns
+        -------
+        dictionary of ephemeris corresponding to ``component``
+        """
+        if component is None:
+            component = self.component_selected
+
+        if component not in self.component.choices:
+            raise ValueError(f"component must be one of {self.component.choices}")
+
+        existing_ephem = self._ephemerides.get(component, {})
+        for name, value in {'t0': t0, 'period': period, 'dpdt': dpdt}.items():
+            if value is not None:
+                existing_ephem[name] = value
+                if component == self.component_selected:
+                    setattr(self, name, value)
+
+        self._ephemerides[component] = existing_ephem
+        return existing_ephem
+
     @observe('period', 'dpdt', 't0')
-    def _period_changed(self, *args):
+    def _ephem_traitlet_changed(self, event={}):
+        if self._ignore_ephem_change:
+            return
         for value in (self.period, self.dpdt, self.t0):
             if not isinstance(value, (int, float)):
                 return
@@ -147,6 +225,10 @@ class Ephemeris(PluginTemplateMixin):
 
         def round_to_1(x):
             return round(x, -int(np.floor(np.log10(abs(x)))))
+
+        # update value in the dictionary (to support multi-ephems)
+        if event:
+            self.update_ephemeris(**{event.get('name'): event.get('new')})
 
         self._update_all_phase_arrays()
 
