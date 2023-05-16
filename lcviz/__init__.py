794469c49b4f1591d6554f4bee923cfdf7443628
Kyle Conroy <kyleconroy@gmail.com>
2023-05-16 08:58:13 -0400

    point shortcut tool to modified plugin

--- lcviz/__init__.py
+++ lcviz/__init__.py
@@ -12,5 +12,6 @@ from . import state  # noqa
 # Expose subpackage API at package level.
 from .plugins import *  # noqa
 from .parsers import *  # noqa
+from .tools import *  # noqa
 from .viewers import *  # noqa
 from .helper import *  # noqa
