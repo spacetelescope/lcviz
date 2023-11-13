de7e411d9be7f9dcfe7b2fa28a1434b7ef0e2d28
Brett M. Morris <bmmorris@stsci.edu>
2023-11-13 13:58:17 -0500

    expose marker color column in API

--- lcviz/plugins/plot_options/plot_options.py
+++ lcviz/plugins/plot_options/plot_options.py
@@ -16,7 +16,7 @@ class PlotOptions(PlotOptions):
         api._expose += ['marker_visible', 'marker_fill', 'marker_opacity',
                         'marker_size_mode', 'marker_size', 'marker_size_scale',
                         'marker_size_col', 'marker_size_vmin', 'marker_size_vmax',
-                        'marker_color_mode', 'marker_color',
+                        'marker_color_mode', 'marker_color', 'marker_color_col',
                         'marker_colormap', 'marker_colormap_vmin', 'marker_colormap_vmax']
 
         return api
