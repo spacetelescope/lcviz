9fff32e615fc06bccb5edf853851ef0d79a9a621
Kyle Conroy <kyleconroy@gmail.com>
2023-12-07 13:19:08 -0500

    enable lcviz.test()

--- lcviz/__init__.py
+++ lcviz/__init__.py
@@ -1,5 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import os
+from astropy.tests.runner import TestRunner
+
 try:
     from .version import version as __version__
 except ImportError:
@@ -15,3 +18,6 @@ from .parsers import *  # noqa
 from .tools import *  # noqa
 from .viewers import *  # noqa
 from .helper import *  # noqa
+
+# Create the test function for self test
+test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
