56b2ea82284155d3ef88b0b84c75ca613e66ace4
Kyle Conroy <kyleconroy@gmail.com>
2023-12-11 09:45:32 -0500

    expose line traitlets through plot options user API (#49)

--- lcviz/plugins/plot_options/plot_options.py
+++ lcviz/plugins/plot_options/plot_options.py
@@ -17,6 +17,7 @@ class PlotOptions(PlotOptions):
                         'marker_size_mode', 'marker_size', 'marker_size_scale',
                         'marker_size_col', 'marker_size_vmin', 'marker_size_vmax',
                         'marker_color_mode', 'marker_color', 'marker_color_col',
-                        'marker_colormap', 'marker_colormap_vmin', 'marker_colormap_vmax']
+                        'marker_colormap', 'marker_colormap_vmin', 'marker_colormap_vmax',
+                        'line_visible', 'line_width']
 
         return api
