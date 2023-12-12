c2c999672d38785157afad571a8b977909aa0631
Kyle Conroy <kyleconroy@gmail.com>
2023-12-12 09:59:58 -0500

    fix target for export plot shortcut button

--- lcviz/tools.py
+++ lcviz/tools.py
@@ -1,5 +1,5 @@
-from jdaviz.core.tools import SidebarShortcutPlotOptions
-
+from jdaviz.core.tools import SidebarShortcutPlotOptions, SidebarShortcutExportPlot
 
 # point to the lcviz-version of plot options instead of jdaviz's
 SidebarShortcutPlotOptions.plugin_name = 'lcviz-plot-options'
+SidebarShortcutExportPlot.plugin_name = 'lcviz-export-plot'
