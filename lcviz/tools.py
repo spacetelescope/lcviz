b1e807ea4a429b3f15573cf9a5fb1a53b67f87d7
Kyle Conroy <kyleconroy@gmail.com>
2023-12-12 14:02:14 -0500

    implement ability to clone viewers
    * NOTE: known bug with layer options reverting when adding to any new viewer (was not introduced here, but this definitely exposes it more readily)

--- lcviz/tools.py
+++ lcviz/tools.py
@@ -1,5 +1,26 @@
+import os
+
+from glue.config import viewer_tool
+from glue.viewers.common.tool import Tool
+
 from jdaviz.core.tools import SidebarShortcutPlotOptions, SidebarShortcutExportPlot
+
+ICON_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), 'data', 'icons'))
 
 # point to the lcviz-version of plot options instead of jdaviz's
 SidebarShortcutPlotOptions.plugin_name = 'lcviz-plot-options'
 SidebarShortcutExportPlot.plugin_name = 'lcviz-export-plot'
+
+
+__all__ = ['ViewerClone']
+
+
+@viewer_tool
+class ViewerClone(Tool):
+    icon = os.path.join(ICON_DIR, 'viewer_clone')
+    tool_id = 'lcviz:viewer_clone'
+    action_text = 'Clone viewer'
+    tool_tip = 'Clone this viewer'
+
+    def activate(self):
+        self.viewer.clone_viewer()
