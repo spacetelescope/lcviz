59565011912f8eafba262267e8e8092f9894889e
Ricky O'Steen <rosteen@stsci.edu>
2024-03-05 16:58:21 -0500

    Compare y_max, which doesn't get rounded by viewer (?)

--- lcviz/tests/test_viewers.py
+++ lcviz/tests/test_viewers.py
@@ -5,18 +5,18 @@ def test_reset_limits(helper, light_curve_like_kepler_quarter):
 
     orig_xlims = (tv.state.x_min, tv.state.x_max)
     orig_ylims = (tv.state.y_min, tv.state.y_max)
-    # set xmin and ymin to midpoints
+    # set xmin and ymax to midpoints
     new_xmin = (tv.state.x_min + tv.state.x_max) / 2
-    new_ymin = (tv.state.y_min + tv.state.y_max) / 2
+    new_ymax = (tv.state.y_min + tv.state.y_max) / 2
     tv.state.x_min = new_xmin
-    tv.state.y_min = new_ymin
+    tv.state.y_max = new_ymax
 
     tv.state._reset_x_limits()
     assert tv.state.x_min == orig_xlims[0]
-    assert tv.state.y_min == new_ymin
+    assert tv.state.y_max == new_ymax
 
     tv.state._reset_y_limits()
-    assert tv.state.y_min == orig_ylims[0]
+    assert tv.state.y_max == orig_ylims[1]
 
 
 def test_clone(helper, light_curve_like_kepler_quarter):
