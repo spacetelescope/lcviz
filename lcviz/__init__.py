68f454c206556cad5b5d49c2ea01581defb8d24e
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-02-18 18:47:09 -0500

    Remove astropy test runner

--- lcviz/__init__.py
+++ lcviz/__init__.py
@@ -1,8 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import os
-from astropy.tests.runner import TestRunner
-
 try:
     from .version import version as __version__
 except ImportError:
@@ -18,6 +15,3 @@ from .parsers import *  # noqa
 from .tools import *  # noqa
 from .viewers import *  # noqa
 from .helper import *  # noqa
-
-# Create the test function for self test
-test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
