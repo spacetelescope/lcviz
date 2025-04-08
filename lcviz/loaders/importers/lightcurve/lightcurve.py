a5b8dea7421d3803c559e33419943270885db082
Kyle Conroy <kyleconroy@gmail.com>
2025-04-08 16:33:52 -0400

    extension multiselect

--- lcviz/loaders/importers/lightcurve/lightcurve.py
+++ lcviz/loaders/importers/lightcurve/lightcurve.py
@@ -1,9 +1,14 @@
+import os
+import numpy as np
 from functools import cached_property
-from traitlets import Bool, List, Unicode, observe
+from traitlets import Any, Bool, List, Unicode, observe
 from astropy.io import fits
 from astropy.table import Table
 from lightkurve import LightCurve
 
+from glue_jupyter.common.toolbar_vuetify import read_icon
+
+from jdaviz.core.tools import ICON_DIR
 from jdaviz.core.registries import loader_importer_registry
 from jdaviz.core.loaders.importers import BaseImporterToDataCollection
 from jdaviz.core.template_mixin import SelectFileExtensionComponent
@@ -33,6 +38,34 @@ def hdu_is_valid(hdu):
             'LC_INIT_ERR' in hdu.columns.names and
             'TUNIT1' in hdu.header)
 
+def hdulist_to_lightcurve(pri_header, hdu):
+    data = Table(hdu.data)
+    # don't load some columns with names that may
+    # conflict with components generated later by lcviz
+    for col in ('PHASE', 'CADENCENO'):
+        if col in data.columns:
+            data.remove_column(col)
+    # Remove rows that have NaN data
+    data = data[~np.isnan(data['LC_INIT'])]
+    time_offset = int(hdu.header['TUNIT1'].split('- ')[1].split(',')[0])
+    data['TIME'] += time_offset
+    lc = LightCurve(data=data,
+                    time=data['TIME'],
+                    flux=data['LC_INIT'],
+                    flux_err=data['LC_INIT_ERR'])
+    lc.meta = dict(pri_header)
+    lc.meta = lc.meta | dict(hdu.header)
+    lc.meta['MISSION'] = 'TESS DVT'
+    lc.meta['FLUX_ORIGIN'] = "LC_INIT"
+    lc.meta['EXTNAME'] = hdu.header['EXTNAME']
+
+    return lc
+
+def has_ephem(lc):
+    return ('TPERIOD' in lc.meta and
+            'TEPOCH' in lc.meta and
+            'TUNIT1' in lc.meta)
+
 
 @loader_importer_registry('Light Curve')
 class LightCurveImporter(BaseImporterToDataCollection):
@@ -44,7 +77,11 @@ class LightCurveImporter(BaseImporterToDataCollection):
     # HDUList-specific options
     input_hdulist = Bool(False).tag(sync=True)
     extension_items = List().tag(sync=True)
-    extension_selected = Unicode().tag(sync=True)
+    extension_selected = Any().tag(sync=True)
+    extension_multiselect = Bool(True).tag(sync=True)
+
+    icon_radialtocheck = Unicode(read_icon(os.path.join(ICON_DIR, 'radialtocheck.svg'), 'svg+xml')).tag(sync=True)  # noqa
+    icon_checktoradial = Unicode(read_icon(os.path.join(ICON_DIR, 'checktoradial.svg'), 'svg+xml')).tag(sync=True)  # noqa
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -59,9 +96,10 @@ class LightCurveImporter(BaseImporterToDataCollection):
             self.extension = SelectFileExtensionComponent(self,
                                                           items='extension_items',
                                                           selected='extension_selected',
+                                                          multiselect='extension_multiselect',
                                                           manual_options=self.input,
                                                           filters=[hdu_is_valid])
-            self._extension_selected_changed()
+            self.extension.select_all()
             # NOTE: data_label_default handled by changes to extension_selected
         else:
             self.data_label_default = self.input.meta.get('OBJECT', 'Light curve')
@@ -86,12 +124,15 @@ class LightCurveImporter(BaseImporterToDataCollection):
     def _extension_selected_changed(self, event={}):
         if not hasattr(self, 'extension') or not self.input_hdulist:
             return
-        self.data_label_default = f"{self.input[0].header.get('OBJECT', 'Light curve')} [{self.extension.selected_item['name']}]"  # noqa
+
         self._clear_cache('output')
 
-        self.create_ephemeris_available = ('TPERIOD' in self.output.meta and
-                                           'TEPOCH' in self.output.meta and
-                                           'TUNIT1' in self.output.meta)
+        if self.extension_multiselect:
+            self.data_label_default = self.input[0].header.get('OBJECT', 'Light curve')
+            self.create_ephemeris_available = any([has_ephem(lc) for lc in self.output])
+        else:
+            self.data_label_default = f"{self.input[0].header.get('OBJECT', 'Light curve')} [{self.extension.selected_item['name']}]"  # noqa
+            self.create_ephemeris_available = has_ephem(self.output)
 
     @property
     def default_viewer_label(self):
@@ -108,43 +149,32 @@ class LightCurveImporter(BaseImporterToDataCollection):
         if isinstance(self.input, LightCurve):
             return self.input
 
-        # HDUList
-        hdulist = self.input
-        hdu = self.extension.selected_hdu
-        data = Table(hdu.data)
-        # don't load some columns with names that may
-        # conflict with components generated later by lcviz
-        for col in ('PHASE', 'CADENCENO'):
-            if col in data.columns:
-                data.remove_column(col)
-        # Remove rows that have NaN data
-        data = data[~np.isnan(data['LC_INIT'])]
-        header = hdu.header
-        time_offset = int(header['TUNIT1'].split('- ')[1].split(',')[0])
-        data['TIME'] += time_offset
-        lc = LightCurve(data=data,
-                        time=data['TIME'],
-                        flux=data['LC_INIT'],
-                        flux_err=data['LC_INIT_ERR'])
-        lc.meta = dict(hdulist[0].header)
-        lc.meta = lc.meta | dict(hdu.header)
-        lc.meta['MISSION'] = 'TESS DVT'
-        lc.meta['FLUX_ORIGIN'] = "LC_INIT"
-        lc.meta['EXTNAME'] = header['EXTNAME']
-
-        return lc
+        pri_header = self.input[0].header
+        if self.extension_multiselect:
+            return [hdulist_to_lightcurve(pri_header, hdu) for hdu in self.extension.selected_hdu]
+        return hdulist_to_lightcurve(pri_header, self.extension.selected_hdu)
 
     def __call__(self):
-        super().__call__()
+        if self.extension_multiselect:
+            data_label = self.data_label_value
+            lcs = self.output
+            with self.app._jdaviz_helper.batch_load():
+                for lc, ext in zip(lcs, self.extension.selected_name):
+                    self.add_to_data_collection(lc, f"{data_label} [{ext}]", show_in_viewer=True)
+        else:
+            super().__call__()
+            lcs = [self.output]
 
         if self.create_ephemeris_available and self.create_ephemeris \
                 and 'Ephemeris' in self.app._jdaviz_helper.plugins:
-            time_offset = int(self.output.meta.get('TUNIT1').split('- ')[1].split(',')[0])
-            period = self.output.meta.get('TPERIOD', 1.0)
-            t0 = self.output.meta.get('TEPOCH', None) + time_offset - self.app.data_collection[0].coords.reference_time.jd  # noqa
+            for lc, ext in zip(lcs, self.extension.selected_name):
+                if not has_ephem(lc):
+                    continue
+                time_offset = int(lc.meta.get('TUNIT1').split('- ')[1].split(',')[0])
+                period = lc.meta.get('TPERIOD', 1.0)
+                t0 = lc.meta.get('TEPOCH', None) + time_offset - self.app.data_collection[0].coords.reference_time.jd  # noqa
 
-            ephem = self.app._jdaviz_helper.plugins['Ephemeris']
-            ephem_component = self.extension.selected_item['name']
-            ephem.add_component(ephem_component, set_as_selected=False)
-            ephem.update_ephemeris(ephem_component, t0=t0, period=period, wrap_at=0.5)
-            ephem.create_phase_viewer(ephem_component)
+                ephem = self.app._jdaviz_helper.plugins['Ephemeris']
+                ephem.add_component(ext, set_as_selected=False)
+                ephem.update_ephemeris(ext, t0=t0, period=period, wrap_at=0.5)
+                ephem.create_phase_viewer(ext)
--- lcviz/loaders/importers/lightcurve/lightcurve.vue
+++ lcviz/loaders/importers/lightcurve/lightcurve.vue
@@ -1,9 +1,21 @@
 <template>
   <v-container>
+    <div style="position: absolute; width: 32px; right: 0px; margin-right: 12px; margin-top: -32px">
+      <j-tooltip tooltipcontent='Toggle multiselect for extension.'>
+        <v-btn
+          icon
+          style="opacity: 0.7"
+          @click="extension_multiselect = !extension_multiselect"
+        >
+          <img :src="extension_multiselect ? icon_checktoradial : icon_radialtocheck" width="24" class="invert-if-dark"/>
+      </v-btn>
+      </j-tooltip>
+    </div>
     <plugin-select
       v-if="input_hdulist"
       :items="extension_items.map(i => i.label)"
       :selected.sync="extension_selected"
+      :multiselect="extension_multiselect"
       :show_if_single_entry="true"
       label="Extension"
       api_hint="ldr.extension ="
@@ -16,19 +28,19 @@
         :default="data_label_default"
         :auto.sync="data_label_auto"
         :invalid_msg="data_label_invalid_msg"
-        label="Data Label"
+        :label="extension_multiselect ? 'Base Data Label' : 'Data Label'"
         api_hint="ldr.importer.data_label ="
         :api_hints_enabled="api_hints_enabled"
-        hint="Label to assign to the new data entry."
+        :hint="extension_multiselect ? 'Base label to assign to new data entries (will include extension as suffix).' : 'Label to assign to the new data entry.'"
       ></plugin-auto-label>
     </v-row>
     <plugin-switch
       v-if="create_ephemeris_available"
       :value.sync="create_ephemeris"
-      label="Create Ephemeris"
-      api_hint="plg.create_ephemeris = "
+      :label="extension_multiselect && extension_selected.length > 1 ? 'Create Ephemerides': 'Create Ephemeris'"
+      api_hint="ldr.importer.create_ephemeris = "
       :api_hints_enabled="api_hints_enabled"
-      hint="Create ephemeris entry and phase-viewer."
+      :hint="extension_multiselect && extension_selected.length > 1 ? 'Create ephemerides entries and phase-viewers.': 'Create ephemeris entry and phase-viewer.'"
     />
   </v-contatiner>
 </template>
