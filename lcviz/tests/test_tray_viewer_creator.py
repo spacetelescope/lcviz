0926293dc1e5b4c30cdccc9df352aaa753113aaa
Kyle Conroy <kyleconroy@gmail.com>
2026-09-17 08:20:42 -0400

    update test
    format needs to be specified since input now also matches "Spectral Lines"

--- lcviz/tests/test_tray_viewer_creator.py
+++ lcviz/tests/test_tray_viewer_creator.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.remote_data
 def test_tray_viewer_creator(helper, light_curve_like_kepler_quarter):
     # additional coverage in test_plugin_ephemeris
-    helper.load(light_curve_like_kepler_quarter)
+    helper.load(light_curve_like_kepler_quarter, format='Light Curve')
     vc = helper._tray_tools['g-viewer-creator']
 
     assert len(helper.viewers) == 1
